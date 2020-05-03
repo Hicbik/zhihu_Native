6b6b6135e9a220792294626dfcdce0da597797f8
Hicbik <1959453433@qq.com>
2020-05-03 17:28:54 +0800

    删除文件 Desktop

--- Desktop/zhihu/zhihu_h5/src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import {applyMiddleware, createStore} from 'redux'
-import {createLogger} from 'redux-logger'
-import rootReducer from './reducer'
-
-const store = () => createStore(rootReducer, applyMiddleware(createLogger()))
-
-export default store()
